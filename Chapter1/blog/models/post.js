ad1e117680f45b453939f2f627cd8c0f63a9b555
lengly <liu_yuda@163.com>
2016-01-16 02:32:39 +0800

    1.10 增加pv统计和留言统计

--- Chapter1/blog/models/post.js
+++ Chapter1/blog/models/post.js
@@ -29,7 +29,8 @@ Post.prototype.save = function(callback) {
 		title: this.title,
 		tags: this.tags,
 		post: this.post,
-		comments: []
+		comments: [],
+		pv: 0
 	};
 	//打开数据库
 	mongodb.open(function(err, db) {
@@ -115,16 +116,31 @@ Post.getOne = function(name, day, title, callback) {
 				"time.day": day,
 				"title": title
 			}, function(err, doc) {
-				mongodb.close();
 				if (err) {
+					mongodb.close();
 					return callback(err); //失败
 				}
-				//解析 markdown 为 html
-				doc.post = markdown.toHTML(doc.post);
-				doc.comments.forEach(function(comment) {
-					comment.content = markdown.toHTML(comment.content);
-				});
-				callback(null, doc); //返回查询的一篇文章
+				if (doc) {
+					//每访问1次  pv值加1
+					collection.update({
+						"name": name,
+						"time.day": day,
+						"title": title
+					}, {
+						$inc: {"pv": 1}
+					}, function(err) {
+						mongodb.close();
+						if (err) {
+							return callback(err);
+						}
+					});
+					//解析 markdown 为 html
+					doc.post = markdown.toHTML(doc.post);
+					doc.comments.forEach(function(comment) {
+						comment.content = markdown.toHTML(comment.content);
+					});
+					callback(null, doc); //返回查询的一篇文章
+				}
 			});
 		});
 	});
