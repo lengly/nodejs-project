45ee29c171a5280c6083f391ecc5b28e13fcb2cb
lengly <liu_yuda@163.com>
2016-01-16 02:21:44 +0800

    1.9 增加标签和标签页面

--- Chapter1/blog/models/post.js
+++ Chapter1/blog/models/post.js
@@ -1,9 +1,10 @@
 var mongodb = require('./db');
 var markdown = require('markdown').markdown;
 
-function Post(name, title, post) {
+function Post(name, title, tags, post) {
 	this.name = name;
 	this.title = title;
+	this.tags = tags;
 	this.post = post;
 }
 
@@ -26,6 +27,7 @@ Post.prototype.save = function(callback) {
 		name: this.name,
 		time: time,
 		title: this.title,
+		tags: this.tags,
 		post: this.post,
 		comments: []
 	};
@@ -236,6 +238,62 @@ Post.getArchive = function(callback) {
 				"name": 1,
 				"time": 1,
 				"title": 1
+			}).sort({
+				time: -1
+			}).toArray(function(err, docs) {
+				mongodb.close();
+				if (err) {
+					return callback(err);
+				}
+				callback(null, docs);
+			});
+		});
+	});
+}
+
+//返回所有标签
+Post.getTags = function(callback) {
+	mongodb.open(function(err, db) {
+		if (err) {
+			return callback(err);
+		}
+		//读取posts集合
+		db.collection('posts', function(err, collection) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			}
+			//distinct用来找出给定键的所有不同值
+			collection.distinct("tags", function(err, docs) {
+				mongodb.close();
+				if (err) {
+					return callback(err);
+				}
+				callback(null, docs);
+			});
+		});
+	});
+}
+
+Post.getTag = function(tag, callback) {
+	mongodb.open(function(err, db) {
+		if (err) {
+			return callback(err);
+		}
+		//读取posts集合
+		db.collection('posts', function(err, collection) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			}
+			//查询并返回所有tags数组内包含tag的文档
+			//并返回只含有name time title组成的数组
+			collection.find({
+				"tags": tag
+			}, {
+				"name": 1,
+				"time": 1,
+				"title": 1,
 			}).sort({
 				time: -1
 			}).toArray(function(err, docs) {
--- Chapter1/blog/routes/index.js
+++ Chapter1/blog/routes/index.js
@@ -122,8 +122,9 @@ module.exports = function(app) {
 
 	app.post('/post', checkLogin);
 	app.post('/post', function(req, res) {
-		var currentUser = req.session.user;
-		var post = new Post(currentUser.name, req.body.title, req.body.post);
+		var currentUser = req.session.user,
+			tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+			post = new Post(currentUser.name, req.body.title, tags, req.body.post);
 		post.save(function(err) {
 			if (err) {
 				req.flash('error', err);
@@ -165,6 +166,38 @@ module.exports = function(app) {
 			}
 			res.render('archive', {
 				title: '存档',
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
+	app.get('/tags', function(req, res) {
+		Post.getTags(function(err, posts) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			res.render('tags', {
+				title: '标签',
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
+	app.get('/tags/:tag', function(req, res) {
+		Post.getTag(req.params.tag, function(err, posts) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			res.render('tag', {
+				title: 'TAG: ' + req.params.tag,
 				posts: posts,
 				user: req.session.user,
 				success: req.flash('success').toString(),
