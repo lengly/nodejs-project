42c2790b6fa7b7b0f370eb6ed4cb070872019600
lengly <liu_yuda@163.com>
2016-01-16 18:42:55 +0800

    1.16 增加日志功能

--- Chapter1/blog/app.js
+++ Chapter1/blog/app.js
@@ -12,6 +12,9 @@ var multer = require('multer');
 var routes = require('./routes/index');
 var settings = require('./settings');
 
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log', {flags: 'a'});
+var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
 var app = express();
 
 app.set('port', process.env.PORT || 3000)
@@ -20,10 +23,16 @@ app.set('view engine', 'ejs');
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(logger({stream: accessLog}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(err, req, res, next) {
+  var meta = '[' + new Date() + ']' + req.url + '\n';
+  errorLog.write(meta + err.stack + '\n');
+  next();
+});
 app.use(session({
   secret: settings.cookieSecret,
   key: settings.db, //cookie name
