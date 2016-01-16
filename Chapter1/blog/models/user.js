5c5869cc2e53a2510c7021f566246111ded9292a
lengly <liu_yuda@163.com>
2016-01-16 16:20:15 +0800

    1.14 增加用户头像

--- Chapter1/blog/models/user.js
+++ Chapter1/blog/models/user.js
@@ -1,4 +1,5 @@
 var mongodb = require('./db');
+var crypto = require('crypto');
 
 function User(user) {
 	this.name = user.name;
@@ -10,11 +11,15 @@ module.exports = User;
 
 //存储用户信息
 User.prototype.save = function(callback) {
+	var md5 = crypto.createHash('md5'),
+		email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+		head = "https://secure.gravatar.com/avatar/" + email_MD5 + "?s=48";
 	//要存入数据库的用户文档
 	var user = {
 		name: this.name,
 		password: this.password,
-		email: this.email
+		email: this.email,
+		head: head
 	};
 	//打开数据库
 	mongodb.open(function(err, db) {
--- Chapter1/blog/routes/index.js
+++ Chapter1/blog/routes/index.js
@@ -124,7 +124,7 @@ module.exports = function(app) {
 	app.post('/post', function(req, res) {
 		var currentUser = req.session.user,
 			tags = [req.body.tag1, req.body.tag2, req.body.tag3],
-			post = new Post(currentUser.name, req.body.title, tags, req.body.post);
+			post = new Post(currentUser.name, currentUser.head, req.body.title, tags, req.body.post);
 		post.save(function(err) {
 			if (err) {
 				req.flash('error', err);
@@ -279,8 +279,12 @@ module.exports = function(app) {
 		var date = new Date(),
 			time =  date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
 			 	+ " " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+		var md5 = crypto.createHash('md5'),
+			email_MD5 = md5.update(req.body.email.toLowerCase()).digest('hex'),
+			head = "https://secure.gravatar.com/avatar/" + email_MD5 + "?s=48";
 		var comment = {
 			name: req.body.name,
+			head: head,
 			email: req.body.email,
 			website: req.body.website,
 			time: time,
