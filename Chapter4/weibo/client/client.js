d657aca7a6c8a1a2de5dcbe54b8dbdf9d6fcdedb
lengly <liu_yuda@163.com>
2016-01-30 19:02:41 +0800

    4.6 用户注册

--- Chapter4/weibo/client/client.js
+++ Chapter4/weibo/client/client.js
@@ -1,3 +1,5 @@
+Session.setDefault("info", {success: "", error: ""});
+
 Template.container.helpers({
 	currentUrl : function() {
 		return Session.get("currentUrl");
@@ -10,20 +12,48 @@ Template.nav.helpers({
 	}
 });
 
+Template.info.helpers({
+	info: function() {
+		return Session.get("info");
+	}
+});
+
 var urlRouter = Backbone.Router.extend({
 	routes: {
 		"": "index",
 		"login": "login",
-		"reg": "reg"
+		"reg": "reg",
+		"logout": "logout"
 	},
 	index: function() {
 		Session.set("currentUrl", {index: "active", login: "", reg: ""});
 	},
 	login: function() {
+		if (Meteor.userId()) {
+			this.navigate("/", true);
+			Session.set("info", {success: "", error: "用户已在线"});
+			return;
+		}
 		Session.set("currentUrl", {index: "", login: "active", reg: ""});
 	},
 	reg: function() {
+		if (Meteor.userId()) {
+			this.navigate("/", true);
+			Session.set("info", {success: "", error: "用户已在线"});
+			return;
+		}
 		Session.set("currentUrl", {index: "", login: "", reg: "active"});
+	},
+	logout: function() {
+		if (Meteor.userId()) {
+			Meteor.logout();
+			this.navigate("/", true);
+			Session.set("info", {success: "登出成功", error: ""});
+			return;
+		} else {
+			this.navigate("/", true);
+			Session.set("info", {success: "", error: "用户不在线"});
+		}
 	},
 	redirect: function(url) {
 		this.navigate(url, true);
@@ -35,4 +65,30 @@ Router = new urlRouter;
 
 Meteor.startup(function() {
 	Backbone.history.start({pushState: true});
+});
+
+Template.reg.events({
+	'click #submit': function(evt) {
+		evt.preventDefault();
+		var $username = $("#username").val();
+		var $password = $("#password").val();
+		var $password_repeat = $("#password-repeat").val();
+		if ($password.length === 0 || $username.length === 0) {
+			Session.set("info", {success: "", error: "用户名或密码不能为空"});
+			return;
+		}
+		if ($password !== $password_repeat) {
+			Session.set("info", {success: "", error: "两次输入密码不一致"});
+			return;
+		}
+
+		Accounts.createUser({username: $("#username").val(), password: $("#password").val()}, function(err) {
+			if (err) {
+				Session.set("info", {success: "", error: err.reason});
+			} else {
+				Router.redirect("/");// 跳转至主页
+				Session.set("info", {success: "注册成功", error: ""});
+			}
+		});
+	}
 });
